30339d04075af875787afafecbf0c28d0ba52573
Ivo Lima Silva <ivolimasilva@outlook.com>
2017-08-02 17:35:45 +0100

    Homepage: Toolbar, Hero and Methodology.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,62 @@
 <template>
-    <div class="white">
+    <div>
+    
+        <!-- Toolbar : Start -->
         <v-toolbar fixed :class="[toolbar ? 'toolbar--home' : 'transparent elevation-0']" dark>
-            <img src="https://vuetifyjs.com/static/v.png" height="38px">
+            <img src="public/logo.png" height="32px">
             <v-spacer></v-spacer>
             <v-toolbar-items>
                 <v-btn flat column dark to="#">Progress</v-btn>
                 <v-btn flat column dark to="#">Privacy Policy</v-btn>
             </v-toolbar-items>
         </v-toolbar>
+        <!-- Toolbar : End -->
+    
+        <!-- Hero : Start -->
         <section class="elevation-12" id="hero">
-            <v-parallax src="https://vuetifyjs.com/static/doc-images/vbanner.jpg">
+            <v-parallax src="/public/night_plane.jpg" height="650">
                 <v-layout row align-vert-center align-horiz-center>
                     <v-flex class="text-xs-center" xs12>
-                        <h1 class="grey--text text--lighten-4 display-2">Master Thesis</h1>
-                        <div class="grey--text text--lighten-4 headline hidden-xs-only">Hashtag recommendation and popularity prediction for social network images</div>
+                        <h1 class="grey--text text--lighten-4 display-2">
+                            Hashtag recommendation and popularity
+                            <br> prediction for social network images
+                        </h1>
+                        <h2 class="grey--text text--lighten-4 display-1">Ivo Lima da Silva</h2>
                     </v-flex>
                 </v-layout>
             </v-parallax>
         </section>
-        <div style="height: 1000px">
-        </div>
+        <!-- Hero : End -->
+    
+        <!-- Description : Start -->
+        <!-- Description : End -->
+
+        <!-- Methodology : Start -->
+        <section class="elevation-12" id="content">
+            <h1 class="display-3">Methodology</h1>
+            <div class="stepper">
+                <v-stepper>
+                    <v-stepper-header>
+                        <v-stepper-step step="1" editable>Dataset collection</v-stepper-step>
+                        <v-divider></v-divider>
+                        <v-stepper-step step="2" editable>Development</v-stepper-step>
+                        <v-divider></v-divider>
+                        <v-stepper-step step="3" editable>Validation</v-stepper-step>
+                    </v-stepper-header>
+                    <v-stepper-content step="1">
+                        <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+                    </v-stepper-content>
+                    <v-stepper-content step="2">
+                        <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+                    </v-stepper-content>
+                    <v-stepper-content step="3">
+                        <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+                    </v-stepper-content>
+                </v-stepper>
+            </div>
+        </section>
+        <!-- Methodology : End -->
+
     </div>
 </template>
 
@@ -46,13 +83,14 @@ export default {
 </script>
 
 <style scoped>
-.toolbar {
+.toolbar,
+#content {
     padding-left: 10%;
     padding-right: 10%;
 }
 
-.hero {
-    height: 100%;
+#content {
+    padding-top: 50px;
 }
 
 .application .theme--dark.toolbar {
@@ -61,5 +99,13 @@ export default {
 
 .toolbar--home {
     background: #132563
+}
+
+.stepper {
+    box-shadow: none;
+}
+
+.stepper__content {
+    padding: 16px 0px;
 }
 </style>
